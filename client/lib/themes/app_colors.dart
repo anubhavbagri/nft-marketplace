5d22d418cdbb2f8078fc27a8563cce7ce46125d2
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-13 17:15:10 +0530

    feat: add custom app colors

---
+++ client/lib/themes/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:client/utils/hex_color.dart';
+
+class AppColors {
+  const AppColors._();
+  static final primaryColor = HexColor("#7D36D4");
+  static final white = HexColor("#FFFFFF");
+}
