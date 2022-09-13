7da669e3725d9dff1e0c0b6f13ac29f43cdc5743
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-13 18:36:49 +0530

    feat: add gilroy font-family

---
+++ client/lib/themes/app_text_styles.dart
@@ -0,0 +1,9 @@
+class AppTextStyles {
+  const AppTextStyles._();
+
+  static const gilroyRegular = "Gilroy";
+  static const gilroyMedium = "Gilroy-Medium";
+  static const gilroySemiBold = "Gilroy-SemiBold";
+  static const gilroyBold = "Gilroy-Bold";
+  static const gilroyExtraBold = "Gilroy-ExtraBold";
+}
