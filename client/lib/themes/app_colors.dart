78dafb9dd693093747e5b0e9d8c33a14ac544207
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-14 04:12:43 +0530

    feat: init themes partially

--- client/lib/themes/app_colors.dart
+++ client/lib/themes/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:client/utils/hex_color.dart';
 
 class AppColors {
   const AppColors._();
-  static final primaryColor = HexColor("#7D36D4");
+  static final primary = HexColor("#7D36D4");
   static final white = HexColor("#FFFFFF");
+  static final black = HexColor("#000000");
 }
