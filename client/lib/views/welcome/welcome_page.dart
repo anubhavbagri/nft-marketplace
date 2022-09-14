60e1135f82cd5b2c3c4ef1fa77b8a8973c77b087
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-15 01:10:27 +0530

    feat: add skeleton of starting page

--- client/lib/views/welcome/welcome_page.dart
+++ client/lib/views/welcome/welcome_page.dart
@@ -1,6 +1,7 @@
 import 'package:client/constants/app_assets.dart';
 import 'package:client/themes/app_colors.dart';
 import 'package:client/utils/size_config.dart';
+import 'package:client/widgets/primary_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -10,13 +11,27 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: AppColors.black,
-        body: Center(
-          child: SvgPicture.asset(
-            AppAssets.lightWordmark,
-          ),
+    return Scaffold(
+      backgroundColor: AppColors.black,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              AppAssets.lightWordmark,
+            ),
+            // const Spacer(),
+            Align(
+              alignment: Alignment.center,
+              child: PrimaryButton(
+                buttonText: 'Create new wallet',
+                width: SizeConfig.safeHorizontal! * 0.8,
+                height: SizeConfig.safeVertical! * 0.02,
+                onPressed: () {},
+              ),
+            )
+          ],
         ),
       ),
     );
