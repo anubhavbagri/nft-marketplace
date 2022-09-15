00cc927a705eb7c9fe9851c5d0b9c65a49bf2a94
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-16 03:15:08 +0530

    feat: add background & btn to welcome page

--- client/lib/views/welcome/welcome_page.dart
+++ client/lib/views/welcome/welcome_page.dart
@@ -1,5 +1,6 @@
 import 'package:client/constants/app_assets.dart';
-import 'package:client/themes/app_colors.dart';
+import 'package:client/themes/app_dimensions.dart';
+import 'package:client/themes/background.dart';
 import 'package:client/utils/size_config.dart';
 import 'package:client/widgets/primary_button.dart';
 import 'package:flutter/material.dart';
@@ -11,27 +12,41 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return Scaffold(
-      backgroundColor: AppColors.black,
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset(
-              AppAssets.lightWordmark,
+    return PrimaryBackground(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  AppAssets.lightWordmark,
+                ),
+                // AppDimensions.hSizedBox2,
+                PrimaryButton(
+                  iconToSet: Icons.account_balance_wallet,
+                  buttonText: 'Create new wallet',
+                  width: SizeConfig.safeHorizontal! * 0.8,
+                  height: SizeConfig.safeVertical! * 0.02,
+                  onPressed: () {},
+                ),
+                AppDimensions.hSizedBox2,
+                PrimaryButton(
+                  widget: SvgPicture.asset(
+                    AppAssets.metamaskIcon,
+                    height: SizeConfig.safeHorizontal! * 0.04,
+                    width: SizeConfig.safeVertical! * 0.02,
+                  ),
+                  buttonText: 'Connect to wallet',
+                  width: SizeConfig.safeHorizontal! * 0.8,
+                  height: SizeConfig.safeVertical! * 0.02,
+                  onPressed: () {},
+                )
+              ],
             ),
-            // const Spacer(),
-            Align(
-              alignment: Alignment.center,
-              child: PrimaryButton(
-                buttonText: 'Create new wallet',
-                width: SizeConfig.safeHorizontal! * 0.8,
-                height: SizeConfig.safeVertical! * 0.02,
-                onPressed: () {},
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
