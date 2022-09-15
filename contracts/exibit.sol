8912b764f0e0e4cb3b62021c585be23b0f662592
aviralraj20142222@gmail.com <your_email@example.com>
2022-09-16 03:10:15 +0530

    feat: market place contracts

---
+++ contracts/exibit.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+error exibit__PriceMustBeAboveZero();
+error exibit__NotApprovedForMarketPlace();
+error exibit__AlreadyListed(address nftAddress, uint256 tokenId);
+error exibit__NotOwner();
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract exibit {
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+
+    event ItemListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    modifier notListed(
+        address nftAddress,
+        uint256 tokenId,
+        address owner
+    ) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price > 0) {
+            revert exibit__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    modifier isOwner(
+        address nftAddress,
+        uint tokenId,
+        address spender
+    ) {
+        IERC721 nft = IERC721(nftAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (spender != owner) {
+            revert exibit__NotOwner();
+        }
+        _;
+    }
+
+    function listItem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    )
+        external
+        notListed(nftAddress, tokenId, msg.sender)
+        isOwner(nftAddress, tokenId, msg.sender)
+    {
+        if (price <= 0) {
+            revert exibit__PriceMustBeAboveZero();
+        }
+
+        IERC721 nft = IERC721(nftAddress);
+        if (nft.getApproved(tokenId) != address(this)) {
+            revert exibit__NotApprovedForMarketPlace();
+        }
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+}
