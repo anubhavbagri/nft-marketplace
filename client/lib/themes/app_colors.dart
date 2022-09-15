0d251c3df3d5ec428678884a61f5bfb41e05713d
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-16 04:54:49 +0530

    feat: add gradient for text

--- client/lib/themes/app_colors.dart
+++ client/lib/themes/app_colors.dart
@@ -1,8 +1,16 @@
 import 'package:client/utils/hex_color.dart';
+import 'package:flutter/material.dart';
 
 class AppColors {
   const AppColors._();
+
   static final primary = HexColor("#7D36D4");
   static final white = HexColor("#FFFFFF");
   static final black = HexColor("#000000");
+
+  static final Shader purpleGradient = LinearGradient(
+    colors: <Color>[HexColor("#9A58DB"), HexColor("#42039C")],
+  ).createShader(
+    const Rect.fromLTWH(100.0, 0.0, 200.0, 20.0),
+  );
 }
