07e977c6aab34cea3c312548574ae634671a70d3
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-18 23:14:04 +0530

    assets: add lottie files

--- client/lib/constants/app_assets.dart
+++ client/lib/constants/app_assets.dart
@@ -5,4 +5,7 @@ class AppAssets {
   static const backgroundImg = 'assets/images/background.svg';
   static const lightWordmark = 'assets/images/wordmark-logo.svg';
   static const metamaskIcon = 'assets/images/metamask-icon.svg';
+  static const lottieTick = 'assets/tick_transparent.json';
+  static const lottieError = 'assets/error.json';
+  static const lottieLoading = 'assets/loading.json';
 }
