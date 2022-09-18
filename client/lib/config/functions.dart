9a1b12165ce53dcd675bdbaae3f75495fd37fa08
aviralraj20142222@gmail.com <your_email@example.com>
2022-09-19 04:35:38 +0530

    added dart config files

---
+++ client/lib/config/functions.dart
@@ -0,0 +1,20 @@
+const fcreateUser = 'createUser';
+const fcreateCollection = 'createCollection';
+
+// CustomERC721Collection functions
+
+// calls
+
+const fcreator = 'creator';
+const fgetUnlockableContent = 'getUnlockableContent';
+const fnfts = 'nfts';
+
+// sends
+
+const fmintNFT = 'mintNFT';
+const fsetForSale = 'setForSale';
+const fbuyFixedPriceNFT = 'buyFixedPriceNFT';
+const fplaceBid = 'placeBid';
+const fcancelBid = 'cancelBid';
+const fsellBiddingNFT = 'sellBiddingNFT';
+const fmodifyListingMechanism = 'modifyListingMechanism';
