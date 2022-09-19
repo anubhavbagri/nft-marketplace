96f866c8ca9df1a11bc229cecd04ce2b2d18c928
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-19 08:46:06 +0530

    fix: jugaad to retain app state on app refresh

--- client/lib/views/main/main_screen.dart
+++ client/lib/views/main/main_screen.dart
@@ -1,12 +1,15 @@
 import 'package:animate_icons/animate_icons.dart';
 import 'package:camera/camera.dart';
 import 'package:client/controllers/main_screen_controller.dart';
+import 'package:client/controllers/wallet_controller.dart';
+import 'package:client/controllers/welcome_controller.dart';
 import 'package:client/themes/app_colors.dart';
 import 'package:client/utils/size_config.dart';
 import 'package:client/views/main/discover_page.dart';
 import 'package:client/views/main/favorite_page.dart';
 import 'package:client/views/main/profile_page.dart';
 import 'package:client/views/main/search_page.dart';
+import 'package:client/views/welcome/welcome_page.dart';
 import 'package:client/widgets/bottom_bar.dart';
 import 'package:client/widgets/primary_button.dart';
 import 'package:flutter/material.dart';
@@ -27,94 +30,89 @@ class MainScreen extends GetView<MainScreenController> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Obx(
-      () => Scaffold(
-        extendBody: true,
-        body: Stack(
-          children: [
-            pages[controller.tabIndex],
-            Padding(
-              padding: const EdgeInsets.only(bottom: 60),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Visibility(
-                  visible: controller.openModal,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 300),
-                      color: Colors.white.withOpacity(0.1),
-                      height: SizeConfig.safeVertical! * 0.3,
-                      width: SizeConfig.safeHorizontal! * 0.9,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          PrimaryButton(
-                              buttonText: "Select from files",
-                              width: SizeConfig.safeHorizontal! * 0.8,
-                              height: SizeConfig.safeVertical! * 0.06,
-                              buttonColor: AppColors.black,
-                              onPressed: () {}),
-                          PrimaryButton(
-                              buttonText: "Capture Now",
-                              width: SizeConfig.safeHorizontal! * 0.8,
-                              height: SizeConfig.safeVertical! * 0.06,
-                              onPressed: () async {
-                                await availableCameras().then((cameras) {
-                                  Get.toNamed("/camera",
-                                      arguments: cameras.first);
-                                });
-                              }),
-                          // Spacer(),
-                        ],
+      () => (WelcomeController.to.account != '' ||
+              WalletController.to.publicAdr != '')
+          ? Scaffold(
+              extendBody: true,
+              body: Stack(
+                children: [
+                  pages[controller.tabIndex],
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 60),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Visibility(
+                        visible: controller.openModal,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: AnimatedContainer(
+                            duration: const Duration(milliseconds: 300),
+                            color: Colors.white.withOpacity(0.1),
+                            height: SizeConfig.safeVertical! * 0.3,
+                            width: SizeConfig.safeHorizontal! * 0.9,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                PrimaryButton(
+                                    buttonText: "Select from files",
+                                    width: SizeConfig.safeHorizontal! * 0.8,
+                                    height: SizeConfig.safeVertical! * 0.06,
+                                    buttonColor: AppColors.black,
+                                    onPressed: () {}),
+                                PrimaryButton(
+                                    buttonText: "Capture Now",
+                                    width: SizeConfig.safeHorizontal! * 0.8,
+                                    height: SizeConfig.safeVertical! * 0.06,
+                                    onPressed: () async {
+                                      await availableCameras().then((cameras) {
+                                        Get.toNamed("/camera",
+                                            arguments: cameras.first);
+                                      });
+                                    }),
+                                // Spacer(),
+                              ],
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
+                ],
+              ),
+              bottomNavigationBar: BottomBar(
+                index: controller.tabIndex,
+                onChangedTab: controller.changeTabIndex,
+              ),
+              floatingActionButton: FloatingActionButton(
+                backgroundColor: AppColors.black,
+                shape: const StadiumBorder(
+                    side: BorderSide(color: Colors.white, width: 1)),
+                onPressed: () {},
+                child: AnimateIcons(
+                  startIcon: Icons.add,
+                  endIcon: Icons.close,
+                  controller: AnimateIconController(),
+                  startTooltip: 'Icons.add_circle',
+                  endTooltip: 'Icons.add_circle_outline',
+                  onStartIconPress: () {
+                    controller.openModal = !controller.openModal;
+                    return true;
+                  },
+                  onEndIconPress: () {
+                    controller.openModal = !controller.openModal;
+                    return true;
+                  },
+                  duration: const Duration(milliseconds: 500),
+                  startIconColor: AppColors.white,
+                  endIconColor: AppColors.white,
+                  clockwise: false,
                 ),
               ),
-            ),
-          ],
-        ),
-        // Visibility(
-        //   visible: controller.openModal,
-        //   child: AnimatedContainer(
-        //     height: SizeConfig.safeVertical! * 0.9,
-        //     width: SizeConfig.safeHorizontal! * 1,
-        //     duration: Duration(milliseconds: 300),
-        //     color: Colors.white.withOpacity(0.1),
-        //   ),
-        // ),
-        bottomNavigationBar: BottomBar(
-          index: controller.tabIndex,
-          onChangedTab: controller.changeTabIndex,
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: AppColors.black,
-          shape: const StadiumBorder(
-              side: BorderSide(color: Colors.white, width: 1)),
-          onPressed: () {},
-          child: AnimateIcons(
-            startIcon: Icons.add,
-            endIcon: Icons.close,
-            controller: AnimateIconController(),
-            startTooltip: 'Icons.add_circle',
-            endTooltip: 'Icons.add_circle_outline',
-            onStartIconPress: () {
-              controller.openModal = !controller.openModal;
-              return true;
-            },
-            onEndIconPress: () {
-              controller.openModal = !controller.openModal;
-              return true;
-            },
-            duration: const Duration(milliseconds: 500),
-            startIconColor: AppColors.white,
-            endIconColor: AppColors.white,
-            clockwise: false,
-          ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      ),
+              floatingActionButtonLocation:
+                  FloatingActionButtonLocation.centerDocked,
+            )
+          : const WelcomePage(),
     );
   }
 }
