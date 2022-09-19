62d9e78503e1f4dae6ddaf44ddb863be64b969bc
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-19 21:51:36 +0530

    feat: add deployed addresses

--- client/lib/config/config.dart
+++ client/lib/config/config.dart
@@ -1,8 +1,8 @@
 import 'package:client/constants/env.dart';
 
 const exibitAddress = '0xd96fEacE7034E7cFCD90102500a70A9358535dcE';
-const createCollectionAddress = '';
-const priceFeedAddress = '';
+const createCollectionAddress = '0x36B6D522A6c2d3118A1c308525687E50D38C3A55';
+const priceFeedAddress = '0x55605337c1ee7e3e798F422cE1ca09A481424456';
 
 //RPC URL
 const rpcURL = 'https://polygon-mumbai.g.alchemy.com/v2/$rpcApiKey';
