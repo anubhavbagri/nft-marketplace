e7fdd47e6ad118ba0a61ffa752826972fd933b3d
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-19 06:56:43 +0530

    feat: err argument in common error string

--- client/lib/constants/app_strings.dart
+++ client/lib/constants/app_strings.dart
@@ -8,7 +8,7 @@ class AppStrings {
   static const congo = 'Congratulations🥳\nYour account has been created!';
   static const publicAdd = 'Public address :';
   static const walletBal = 'Wallet Balance :';
-  static const error = 'Something went wrong 🤒\nPlease try again later!';
+  static String error(String err) => 'Something went wrong 🤒\n$err';
 
   static const publicAdrInfo =
       'Public address has been copied to clipboard! It would be required to get Test Matic ✨\n\nP.S. Click on the address to copy it again';
