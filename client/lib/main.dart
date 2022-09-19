1711495fea64d2a14b402adaab634114b4a47f59
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-19 06:58:49 +0530

    feat: add binding dependency & init sizeConfig

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:camera/camera.dart';
 import 'package:client/bindings/main_screen_binding.dart';
 import 'package:client/bindings/wallet_binding.dart';
+import 'package:client/bindings/welcome_binding.dart';
 import 'package:client/routes.dart';
 import 'package:client/themes/app_themes.dart';
+import 'package:client/utils/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -11,13 +12,13 @@ import 'package:get_storage/get_storage.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  //setting up camera managery
-  final cameras = await availableCameras();
+  // setting up camera managery
+  // final cameras = await availableCameras();
 
   // await loadServices();
 
-  //Get specific camera from the list of available cameras
-  final firstCamera = cameras.first;
+  // Get specific camera from the list of available cameras
+  // final firstCamera = cameras.first;
 
   loadDependencies();
   await GetStorage.init();
@@ -34,6 +35,7 @@ Future<void> main() async {
 // Future<void> loadServices() async {}
 
 void loadDependencies() {
+  WelcomeBinding().dependencies();
   WalletBinding().dependencies();
   MainScreenBinding().dependencies();
 }
@@ -44,6 +46,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return GetMaterialApp(
       title: 'exibit',
       initialRoute: "/",
