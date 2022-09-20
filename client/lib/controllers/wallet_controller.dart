8ebd5ad0c8aab995962b68568fb38097aef514c2
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-20 22:35:09 +0530

    feat: add logic for computing gas fees

--- client/lib/controllers/wallet_controller.dart
+++ client/lib/controllers/wallet_controller.dart
@@ -1,14 +1,26 @@
 import 'package:client/utils/global_utils.dart';
+import 'package:client/utils/services/contract_service.dart';
+import 'package:client/utils/services/gasprice_service.dart';
 import 'package:client/utils/services/wallet_service.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:web3dart/web3dart.dart';
 
 class WalletController extends GetxController with StateMixin {
   static WalletController get to => Get.find();
+
   final WalletService _walletService = WalletService();
+  final GasPriceService _gasPriceService = GasPriceService();
+  final ContractService _contractService = ContractService();
 
   late Credentials cred;
   late EthereumAddress address;
+
+  GasInfo? gasInfo;
+  Transaction? transactionInfo;
+  double totalAmount = 0;
+  String lastTxHash = '';
+  double maticPrice = 0;
 
   final _publicAdr = ''.obs;
 
@@ -57,6 +69,35 @@ class WalletController extends GetxController with StateMixin {
   void onReady() {
     super.onReady();
     copyAddress();
+  }
+
+  getTransactionFee(Transaction transaction) async {
+    try {
+      transactionInfo = transaction;
+      gasInfo = null;
+
+      gasInfo = await _gasPriceService.getGasInfo(transaction);
+
+      if (transactionInfo!.value == null) {
+        totalAmount = gasInfo!.totalGasRequired;
+      } else {
+        totalAmount = gasInfo!.totalGasRequired +
+            transactionInfo!.value!.getValueInUnit(EtherUnit.ether);
+      }
+
+      final contract = _contractService.priceFeed;
+      final price = await ethereum.call(
+        contract: contract,
+        function: contract.function('getLatestPrice'),
+        params: [],
+      );
+
+      maticPrice = price[0] / BigInt.from(10).pow(8);
+
+      setBalance(address);
+    } catch (e) {
+      debugPrint('Error at WallerController -> GetTransactionFee: $e');
+    }
   }
 
   void copyAddress() {
