6e8c0b7abe1964ba6db627883715d36b5ec02868
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-20 22:30:09 +0530

    feat: add listing info model

---
+++ client/lib/models/listing_info.dart
@@ -0,0 +1,26 @@
+class ListingInfo {
+  late bool forSale;
+  late bool isFixedPrice;
+  final ListingType listingType;
+  final double price;
+  final int royalties;
+
+  ListingInfo({
+    required this.listingType,
+    required this.price,
+    required this.royalties,
+  }) {
+    forSale = listingType == ListingType.fixedPriceSale;
+
+    isFixedPrice = !(listingType == ListingType.bidding);
+
+    // listingType == ListingType.fixedPriceSale ||
+    // listingType == ListingType.fixedPriceNotSale;
+  }
+}
+
+enum ListingType {
+  fixedPriceSale,
+  fixedPriceNotSale,
+  bidding,
+}
