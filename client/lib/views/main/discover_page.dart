7aadad67b3360f52d81b6498930c97ca524bf7ee
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-09-20 22:33:47 +0530

    fix: discover pg UI

--- client/lib/views/main/discover_page.dart
+++ client/lib/views/main/discover_page.dart
@@ -1,9 +1,12 @@
+import 'package:client/controllers/welcome_controller.dart';
 import 'package:client/themes/app_colors.dart';
 import 'package:client/themes/app_text_styles.dart';
 import 'package:client/themes/background.dart';
+import 'package:client/utils/global_utils.dart';
 import 'package:client/utils/size_config.dart';
 import 'package:client/widgets/collection_list.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class DiscoverPage extends StatelessWidget {
   const DiscoverPage({super.key});
@@ -17,7 +20,7 @@ class DiscoverPage extends StatelessWidget {
         body: NestedScrollView(
           headerSliverBuilder: ((context, innerBoxIsScrolled) => [
                 SliverAppBar(
-                  backgroundColor: Colors.transparent.withOpacity(0.1),
+                  backgroundColor: Colors.transparent,
                   // expandedHeight: AppDimensions.heading1TextSize * 0,
                   toolbarHeight: SizeConfig.safeVertical! * 0.12,
                   floating: true,
@@ -27,44 +30,61 @@ class DiscoverPage extends StatelessWidget {
                   flexibleSpace: Container(
                     child: Padding(
                       padding: const EdgeInsets.fromLTRB(30, 60, 20, 0),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Hi, User",
-                            style: AppTextStyles.h3().copyWith(
-                              fontFamily: AppTextStyles.gilroyBold,
-                              foreground: Paint()
-                                ..shader = AppColors.purpleGradient,
-                            ),
-                          ),
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Container(
-                              height: SizeConfig.safeVertical! * 0.05,
-                              width: SizeConfig.safeVertical! * 0.15,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.black.withOpacity(0.3),
-                                border: Border.all(
-                                  color: Colors.white.withOpacity(0.1),
-                                  width: 1,
+                      child: Obx(
+                        () => Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Container(
+                                height: SizeConfig.safeVertical! * 0.05,
+                                width: SizeConfig.safeVertical! * 0.15,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.black.withOpacity(0.3),
+                                  border: Border.all(
+                                    color: Colors.white.withOpacity(0.1),
+                                    width: 1,
+                                  ),
                                 ),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  "0.235 MAT",
-                                  style: AppTextStyles.h5().copyWith(
-                                    fontFamily: AppTextStyles.gilroyBold,
-                                    foreground: Paint()
-                                      ..shader = AppColors.purpleGradient,
+                                child: Center(
+                                  child: Text(
+                                    formatAddress(WelcomeController.to.account),
+                                    style: AppTextStyles.h5().copyWith(
+                                      fontFamily: AppTextStyles.gilroyBold,
+                                      color: AppColors.secondary,
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
-                          )
-                        ],
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Container(
+                                height: SizeConfig.safeVertical! * 0.05,
+                                width: SizeConfig.safeVertical! * 0.15,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.black.withOpacity(0.3),
+                                  border: Border.all(
+                                    color: Colors.white.withOpacity(0.1),
+                                    width: 1,
+                                  ),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '${WelcomeController.to.balance} MAT',
+                                    style: AppTextStyles.h5().copyWith(
+                                      fontFamily: AppTextStyles.gilroyBold,
+                                      color: AppColors.secondary,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
